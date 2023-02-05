25aeda0b5f7a191de9ccb9592acfa72885a8ba08
ProjectAeternus <124256776+ProjectAeternus@users.noreply.github.com>
2023-02-05 16:44:08 +0200

    door

--- FJG2023/Assets/Kaapo/Scripts/scr_door.cs
+++ FJG2023/Assets/Kaapo/Scripts/scr_door.cs
@@ -5,65 +5,37 @@ using UnityEngine;
 public class scr_door : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    public Sprite[] newSprite;
+    public Sprite newSprite;
+    public Animator animator;
     private int leverOpenCount;
-    private Rigidbody2D potatoRigidBody;
-    private Rigidbody2D playerRigidBody;
-    private bool keepOpen;
-    private float smoothMultiplier;
-    private int doorOrder;
+    public int openingOrder; //leave number 0, opens first, 1 the second and so on...
+    public bool death; //as requested
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] doors = GameObject.FindGameObjectsWithTag("tag_door");
-        doorOrder = doors.Length;
 
-        foreach (GameObject door in doors) //check every lever for distance
-        {
-            if (door.transform.position.y > transform.position.y) //if other is higher than I
-            {
-                doorOrder--;
-            }
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
         GameObject[] levers = GameObject.FindGameObjectsWithTag("tag_lever"); //gather all levers in room to this array
-        foreach (GameObject lever in levers) //check every lever for distance
+        foreach (GameObject lever in levers)
         {
             if (lever.GetComponent<scr_lever>().leverPulled == true)
             {
                 leverOpenCount++;
-            }
-        }
-
-        if (leverOpenCount == doorOrder) //my lever is open
-        {
-            Destroy(gameObject);
-        }
-
-        if (leverOpenCount > 2) //3 or more levers on
-        {
-            if (keepOpen == false)
-            {
-                GameObject player = GameObject.FindGameObjectWithTag("Player");
-                GameObject[] potatoes = GameObject.FindGameObjectsWithTag("Potato");
-                foreach (GameObject potato in potatoes)
+                if (leverOpenCount == openingOrder)
                 {
-                    Debug.Log("ZUCC");
-                    Vector3 v3Force = 10f * transform.forward;
-                    potatoRigidBody = potato.GetComponent<Rigidbody2D>();
-                    potato.GetComponent<Rigidbody2D>().AddForce(v3Force);
-
-                    playerRigidBody = player.GetComponent<Rigidbody2D>();
-                    player.transform.position -= new Vector3(0, (1 * smoothMultiplier) * Time.deltaTime, 0);
-                    smoothMultiplier += 0.0025f;
+                    //animaatio (avautuu)
+                }
+                if (leverOpenCount == 2)
+                {
+                    death = true;
                 }
             }
+
         }
     }
 }
--- FJG2023/Assets/Kaapo/Scripts/scr_tentacle.cs
+++ FJG2023/Assets/Kaapo/Scripts/scr_tentacle.cs
@@ -19,6 +19,11 @@ public class scr_tentacle : MonoBehaviour
     private int randomGood;
     public float randomX;
     public float randomY;
+    private float distanceToLever;
+    private float movementSpeed = 0.12f;
+    private bool attachOnce;
+    private float distanceMultiplier;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +40,7 @@ public class scr_tentacle : MonoBehaviour
         {
             positionOffset = 0.1f;
         }
+
         target = new Vector3(transform.position.x + randomX + positionOffset, transform.position.y + randomY + positionOffset, 0);
         lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.startWidth = 0.2f;
@@ -57,51 +63,62 @@ public class scr_tentacle : MonoBehaviour
         if (oneAtATime == true)
         {
             StartCoroutine(readyToFruitBad());
-            StartCoroutine(readyToFruitGood());
+            for (int i = Random.Range(1, 4); i > 0; i--)
+            {
+                StartCoroutine(readyToFruitGood());
+            }
             oneAtATime = false;
         }
-        
+
+        GameObject[] levers = GameObject.FindGameObjectsWithTag("tag_lever"); //gather all levers in room to this array
+        foreach (GameObject lever in levers) //check every lever for distance
+        {
+            distanceToLever = Vector3.Distance(lever.transform.position, vertexPositions[0]);
+            if (distanceToLever < 8f)
+            {
+                target = new Vector3((transform.position.x / lever.transform.position.x), (transform.position.y / lever.transform.position.y), 0);
+                movementSpeed = 0.1f;
+            }
+        }
+        distanceMultiplier = Vector3.Distance(transform.position, vertexPositions[0]);
         vertexPositions[0] = transform.parent.position; //set first segment at parent
         for (int i = 1; i < vertexPositions.Length; i++) //repeat for every segment
         {
-            vertexPositions[i] = Vector3.SmoothDamp(vertexPositions[i], vertexPositions[i - 1] + target, ref vertexSpeed[i], 0.1f); //position current vertex towards last one in direction of targer at set speed
+            vertexPositions[i] = Vector3.SmoothDamp(vertexPositions[i], vertexPositions[i - 1] + target / distanceMultiplier, ref vertexSpeed[i], movementSpeed); //position current vertex towards last one in direction of targer at set speed
         }
         lineRenderer.SetPositions(vertexPositions); //render line
     }
-    
+
     IEnumerator readyToFruitBad()
     {
         randomBad = Random.Range(6, 11);
-        fruitAtRandom = Random.Range(10, 20);
+        fruitAtRandom = Random.Range(6, 15);
         yield return new WaitForSeconds(fruitAtRandom); //wait 20 seconds until fruits sprout
-        GameObject bad_potato = new GameObject ("bad_potato");
+        GameObject bad_potato = new GameObject("bad_potato");
         bad_potato.AddComponent<scr_bad_potato>();
+        bad_potato.AddComponent<BoxCollider2D>();
         SpriteRenderer renderer = bad_potato.AddComponent<SpriteRenderer>();
         renderer.sprite = potatoSprite[randomBad];
         bad_potato.GetComponent<scr_bad_potato>().tentacleMaterial = tentacleMaterial;
         bad_potato.GetComponent<scr_bad_potato>().shadowMaterial = shadowMaterial;
-        //bad_potato.GetComponent<scr_bad_potato>().rigidBody = bad_potato.AddComponent<Rigidbody>();
         bad_potato.GetComponent<scr_bad_potato>().rigidBody = bad_potato.AddComponent<Rigidbody2D>();
+        bad_potato.GetComponent<scr_bad_potato>().player = player;
         bad_potato.GetComponent<scr_bad_potato>().potatoSprite = potatoSprite; //return sprite stored from the parent to child
         bad_potato.transform.parent = gameObject.transform;
     }
-    
+
     IEnumerator readyToFruitGood()
     {
-        for (int i = Random.Range(0, 4); i >= 0; i--)
-        {
-            randomGood = Random.Range(0, 5);
-            fruitAtRandom = Random.Range(10, 20);
-            yield return new WaitForSeconds(fruitAtRandom); //wait 20 seconds until fruits sprout
-            GameObject good_potato = new GameObject("good_potato");
-            good_potato.AddComponent<scr_good_potato>();
-            SpriteRenderer renderer = good_potato.AddComponent<SpriteRenderer>();
-            renderer.sprite = potatoSprite[randomGood];
-            good_potato.GetComponent<scr_good_potato>().shadowMaterial = shadowMaterial;
-            good_potato.GetComponent<scr_good_potato>().rigidBody = good_potato.AddComponent<Rigidbody>();
-            good_potato.GetComponent<scr_good_potato>().potatoSprite = potatoSprite; //return sprite stored from the parent to child
-            good_potato.transform.parent = gameObject.transform;
-        }
+        randomGood = Random.Range(0, 5);
+        fruitAtRandom = Random.Range(6, 15);
+        yield return new WaitForSeconds(fruitAtRandom); //wait 20 seconds until fruits sprout
+        GameObject good_potato = new GameObject("good_potato");
+        good_potato.AddComponent<scr_good_potato>();
+        SpriteRenderer renderer = good_potato.AddComponent<SpriteRenderer>();
+        renderer.sprite = potatoSprite[randomGood];
+        good_potato.GetComponent<scr_good_potato>().shadowMaterial = shadowMaterial;
+        good_potato.GetComponent<scr_good_potato>().rigidBody = good_potato.AddComponent<Rigidbody2D>();
+        good_potato.GetComponent<scr_good_potato>().potatoSprite = potatoSprite; //return sprite stored from the parent to child
+        good_potato.transform.parent = gameObject.transform;
     }
 }
-
